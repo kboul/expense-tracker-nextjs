709c22f67f5d1efe7f15e228ea31b26a199939e1
kboul <kos@voulgaridis@gmail.com>
2024-06-30 14:03:30 +0300

    Init, clean and setup project

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({ weight: "400", subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Expense Tracker",
+  description: "Track your expenses and create a budget"
+};
+
+type RootLayoutProps = Readonly<{
+  children: React.ReactNode;
+}>;
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  );
+}
