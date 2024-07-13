d1bfdf365b2062c0c3e567b05266bf5366c7bc65
kboul <kos.voulgaridis@gmail.com>
2024-07-13 23:06:00 +0300

    Get income and expense from db and replace dummy values on IncomeExpense comp

--- app/actions/index.ts
+++ app/actions/index.ts
@@ -1,2 +1,3 @@
 export * from "./addTransaction";
+export * from "./getIncomeExpense";
 export * from "./getUserBalance";
--- components/IncomeExpense.tsx
+++ components/IncomeExpense.tsx
@@ -1,15 +1,17 @@
+import { getIncomeExpense } from "@/app/actions";
 import React from "react";
 
-export default function IncomeExpense() {
+export default async function IncomeExpense() {
+  const { expense, income } = await getIncomeExpense();
   return (
     <div className="mx-0 my-5 flex justify-evenly bg-white p-5 shadow-sm">
       <div className="text-center">
         <h4>Income</h4>
-        <p className="money text-[#2ecc71]">€ 700</p>
+        <p className="money text-[#2ecc71]">€ {income}</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p className="money mx-0 text-[#c0392b]">€ 200</p>
+        <p className="money mx-0 text-[#c0392b]">€ {expense}</p>
       </div>
     </div>
   );
