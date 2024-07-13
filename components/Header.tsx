75ba15ee0012e2251066f985a9a4b633a965f118
kboul <kos.voulgaridis@gmail.com>
2024-07-13 23:01:49 +0300

    Stretch items on navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ export default async function Header() {
   const user = await checkUser();
   return (
     <nav className="bg-black p-2 text-white">
-      <div className="mx-auto my-0 flex max-w-[900px] items-center justify-between px-[2rem] py-0">
+      <div className="mx-auto my-0 flex max-w-[900px] items-center justify-between py-0">
         <h2>Expense Tracker</h2>
         <div>
           <SignedOut>
