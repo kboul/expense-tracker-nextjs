09119e7db06e21191b658d30d0487fb6efb6100d
Konstantinos Voulgaridis <kos.voulgaridis@gmail.com>
2024-07-22 22:06:27 +0300

    Feature/no transaction message and global styles cleanup (#7)
    * Display a msg when no transactions exist
    
    * Cleanup global.css
    
    * Income expense style improvements
    
    * More global.css refactor and make no transactions alert rounded

--- components/IncomeExpense.tsx
+++ components/IncomeExpense.tsx
@@ -4,14 +4,14 @@ import { formatTransaction } from "@/utils";
 export default async function IncomeExpense() {
   const { expense, income } = await getIncomeExpense();
   return (
-    <div className="mx-0 my-5 flex justify-evenly bg-white p-5 shadow-sm">
+    <div className="my-3 flex justify-around bg-white p-4 shadow-sm">
       <div className="text-center">
         <h4>Income</h4>
         <p className="money text-plusColor">€ {formatTransaction(income)}</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p className="money mx-0 text-minusColor">
+        <p className="money text-center text-minusColor">
           € {formatTransaction(expense)}
         </p>
       </div>
