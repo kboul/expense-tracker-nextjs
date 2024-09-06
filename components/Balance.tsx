9d2c5e8ffd76e8187e0884a18f858f36de322de8
daemon3333 <kos.voulgaridis@gmail.com>
2024-09-06 12:48:12 +0300

    Remove your balance header

--- components/Balance.tsx
+++ components/Balance.tsx
@@ -5,7 +5,6 @@ export default async function Balance() {
   const { balance } = await getUserBalance();
   return (
     <div className="text-center">
-      <h4>Your Balance</h4>
       <h1>€ {formatTransaction(balance) ?? 0}</h1>
     </div>
   );
